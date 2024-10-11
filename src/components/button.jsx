96c85cc82ef106b427020dff42805ad9470bf383
JATIN SAINI <jatinsaini.developer@gmail.com>
2024-10-11 20:28:59 +0530

    add button hover animation

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,44 +1,89 @@
 import "../styles/global.scss";
 import { useState } from "react";
 
-const buttonComponent = () => {
+// Import your SVG assets
+import github from '../assets/svg/githubWhite.svg';
+import figma from '../assets/svg/figma.svg';
+import p5 from '../assets/svg/p5.svg';
+import python from '../assets/svg/python.svg';
+import react from '../assets/svg/react.svg';
+import scss from '../assets/svg/scss.svg';
+import tailwind from '../assets/svg/tailwind.svg';
+import three from '../assets/svg/three.svg';
+import vs from '../assets/svg/vs.svg';
+import vscode from '../assets/svg/vscode.svg';
+import webstrom from '../assets/svg/webstrom.svg'
 
+const ButtonComponent = () => {
   const TechButtons = [
-    { name: "Github", link: "https://github.com", img: "https://www.svgrepo.com/show/530642/folder.svg" },
-    { name: "Figma", link: "https://www.figma.com", img: "https://www.svgrepo.com/show/530642/folder.svg" },
-    { name: "P5.js", link: "https://p5js.org", img: "https://www.svgrepo.com/show/530642/folder.svg" },
-    { name: "Python", link: "https://www.python.org/", img: "https://www.svgrepo.com/show/530642/folder.svg" },
-    { name: "React", link: "https://reactjs.org/", img: "https://www.svgrepo.com/show/530642/folder.svg" },
-    { name: "SCSS", link: "https://sass-lang.com/", img: "https://www.svgrepo.com/show/530642/folder.svg" },
-    { name: "TailwindCSS", link: "https://tailwindcss.com/" },
-    { name: "VsCode", link: "https://threejs.org/", img: "https://www.svgrepo.com/show/530642/folder.svg" },
-    { name: "Github", link: "https://github.com", img: "https://www.svgrepo.com/show/530642/folder.svg" },
-    { name: "Figma", link: "https://www.figma.com", img: "https://www.svgrepo.com/show/530642/folder.svg" },
-    { name: "P5.js", link: "https://p5js.org", img: "https://www.svgrepo.com/show/530642/folder.svg" },
-    { name: "Python", link: "https://www.python.org/", img: "https://www.svgrepo.com/show/530642/folder.svg" },
-    { name: "React", link: "https://reactjs.org/", img: "https://www.svgrepo.com/show/530642/folder.svg" },
-    { name: "SCSS", link: "https://sass-lang.com/", img: "https://www.svgrepo.com/show/530642/folder.svg" },
-    { name: "TailwindCSS", link: "https://tailwindcss.com/", img: "https://www.svgrepo.com/show/530642/folder.svg" },
-    { name: "VsCode", link: "https://threejs.org/", img: "https://www.svgrepo.com/show/530642/folder.svg" },
-    { name: "Github", link: "https://github.com", img: "https://www.svgrepo.com/show/530642/folder.svg" },
-    { name: "Figma", link: "https://www.figma.com", img: "https://www.svgrepo.com/show/530642/folder.svg" },
-    { name: "P5.js", link: "https://p5js.org", img: "https://www.svgrepo.com/show/530642/folder.svg" },
-    { name: "Python", link: "https://www.python.org/", img: "https://www.svgrepo.com/show/530642/folder.svg" },
-    { name: "React", link: "https://reactjs.org/", img: "https://www.svgrepo.com/show/530642/folder.svg" },
-    { name: "SCSS", link: "https://sass-lang.com/", img: "https://www.svgrepo.com/show/530642/folder.svg" },
-
-  ]
-
+    {
+      name: "GitHub",
+      link: "https://github.com",
+      img: github,  // Corrected here
+    },
+    {
+      name: "Figma",
+      link: "https://www.figma.com",
+      img: figma,   // Corrected here
+    },
+    {
+      name: "P5.js",
+      link: "https://p5js.org",
+      img: p5,      // Corrected here
+    },
+    {
+      name: "Python",
+      link: "https://www.python.org/",
+      img: python,  // Corrected here
+    },
+    {
+      name: "React",
+      link: "https://reactjs.org/",
+      img: react,   // Corrected here
+    },
+    {
+      name: "SCSS",
+      link: "https://sass-lang.com/",
+      img: scss,    // Corrected here
+    },
+    {
+      name: "Tailwind CSS",
+      link: "https://tailwindcss.com/",
+      img: tailwind, // Corrected here
+    },
+    {
+      name: "Visual Studio",
+      link: "https://code.visualstudio.com/",
+      img: vs,      // Corrected here
+    },
+    {
+      name: "webstrom",
+      link: "https://code.visualstudio.com/",
+      img: webstrom,      // Corrected here
+    },
+    {
+      name: "vs Code",
+      link: "https://code.visualstudio.com/",
+      img: vscode,      // Corrected here
+    },
+    {
+      name: "Three.js",
+      link: "https://code.visualstudio.com/",
+      img: three,      // Corrected here
+    },
+  ];
 
   const [isHovered, setIsHovered] = useState(false);
   const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+  const [currentImage, setCurrentImage] = useState("");
 
-  const handleMouseEnter = () => {
-    setIsHovered(true);
+  const handleMouseEnter = (img) => {
+    setCurrentImage(img);
   };
 
   const handleMouseLeave = () => {
     setIsHovered(false);
+    setCurrentImage("");
   };
 
   const handleMouseMove = (e) => {
@@ -46,38 +91,33 @@ const buttonComponent = () => {
     setMousePosition({ x: e.pageX, y: e.pageY });
   };
 
-
   return (
-    <div className="tech_buttons" 
-      onMouseEnter={handleMouseEnter}
+    <div
+      className="tech_buttons"
+      onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={handleMouseLeave}
-      onMouseMove={handleMouseMove} >
+      onMouseMove={handleMouseMove}
+    >
       {TechButtons.map((button, index) => (
-        <a key={index} href={button.link} className="tech-button">{button.name}</a>
+        <a key={index} href={button.link} className="tech-button" onMouseEnter={() => handleMouseEnter(button.img)}>
+          {button.name}
+        </a>
       ))}
 
       {/* The pop-up container with image appears on hover at the pointer's position */}
-      {isHovered && (
+      {isHovered && currentImage && (
         <div
           className="popup-container"
           style={{
-            top: mousePosition.y-200 + "px",
-            left: mousePosition.x-100 + "px",
+            top: mousePosition.y - 220 + "px",
+            left: mousePosition.x - 100 + "px",
           }}
         >
-          <img
-            src="https://www.svgrepo.com/show/530642/folder.svg"
-            alt="Related to link"
-            className="popup-image"
-          />
+          <img src={currentImage} alt="Related technology" className="popup-image" /> {/* Added alt attribute */}
         </div>
       )}
-
-
-
-      {/* <a className="tech-button" href="https://github.com/JatinSainiOO7" >React</a> */}
     </div>
   );
 };
 
-export default buttonComponent;
+export default ButtonComponent;
