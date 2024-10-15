27e3c90a05dce0212338f992801ca3cf37967b8c
JATIN SAINI <jatinsaini.developer@gmail.com>
2024-10-15 15:58:21 +0530

    add link container

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,27 @@ import './App.css'
 import Headrer from './components/header.jsx'
 import Main from './components/mainsec.jsx'
 import Footer from './components/footer.jsx'
+import github from './assets/svg/github.svg'
+import linkedin from './assets/svg/linkedin.svg'
+import mail from './assets/svg/mail.svg'
+import twitter from './assets/svg/twitter.svg'
 function App() {
   return (
     <>
+    <div className='social-container'>
+      <a href="https://github.com/JatinSainiOO7">
+        <img className='img-h' src={github} alt="github" />
+      </a>
+      <a href="https://www.linkedin.com/in/jatin-saini-711055294/">
+        <img className='img-h' src={linkedin} alt="LinkedIn" />
+      </a>
+      <a href="https://x.com/JatinSaini0O7">
+        <img className='img-h' src={twitter} alt="twitter" />
+      </a>
+      <a href="https://mail.google.com/mail/u/0/#sent?compose=new">
+        <img className='img-h' src={mail} alt="Mail" />
+      </a>
+    </div>
     <Headrer />
     <Main />
     <Footer />
--- src/components/work.jsx
+++ src/components/work.jsx
@@ -1,5 +1,6 @@
 import '../styles/components.scss'
-
+import code from '../assets/svg/code.svg'
+import api from '../assets/svg/api.svg'
 const WorkSection = () => {
     return (
         <>
@@ -7,22 +8,26 @@ const WorkSection = () => {
             <center><h1 className='work-title '>What I Do_</h1></center>
             <div className='work-card-container'>
                 <div className='card'>
-                    <center><div className="card-img"></div></center>
+                    <center><div className="card-img">
+                        <img src={code} alt="" /></div></center>
                     <center><h1 className='card-title'>Frontend Development</h1></center>
                     <center><p className='card-dis'>Crafting sleek, responsive, and modern web applications</p></center>
                 </div>
                 <div className='card'>
-                    <center><div className="card-img"></div></center>
+                    <center><div className="card-img">
+                    <img src={code} alt="" /></div></center>
                     <center><h1 className='card-title'>Design To Code</h1></center>
                     <center><p className='card-dis'>Transforming Figma designs into pixel-perfect React components</p></center>
                 </div>
                 <div className='card'>
-                    <center><div className="card-img"></div></center>
+                    <center><div className="card-img">
+                    <img src={code} alt="" /></div></center>
                     <center><h1 className='card-title'>UI/UX Design</h1></center>
                     <center><p className='card-dis'>Crafting sleek, responsive, and modern web applications</p></center>
                 </div>
                 <div className='card'>
-                    <center><div className="card-img"></div></center>
+                    <center><div className="card-img">
+                    <img src={api} alt="" /></div></center>
                     <center><h1 className='card-title'>API Integration</h1></center>
                     <center><p className='card-dis'>Implementing robust APIs to enhance dynamic functionalities</p></center>
                 </div>
